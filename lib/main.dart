748967db8e7a25a84b9d748248a9368a0d768b09
Alan Garny <agarny@hellix.com>
2019-12-28 22:38:57 +1300

    Updated the application icon on both Android and iOS.
    And use a burnt colour as our theme's colour.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'WaniKani for Mobile',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: MaterialColor(0xFF294DDC, <int, Color>{
+          50: Color.fromRGBO(41, 77, 220, 0.1),
+          100: Color.fromRGBO(41, 77, 220, 0.2),
+          200: Color.fromRGBO(41, 77, 220, 0.3),
+          300: Color.fromRGBO(41, 77, 220, 0.4),
+          400: Color.fromRGBO(41, 77, 220, 0.5),
+          500: Color.fromRGBO(41, 77, 220, 0.6),
+          600: Color.fromRGBO(41, 77, 220, 0.7),
+          700: Color.fromRGBO(41, 77, 220, 0.8),
+          800: Color.fromRGBO(41, 77, 220, 0.9),
+          900: Color.fromRGBO(41, 77, 220, 1.0),
+        }),
       ),
       home: MyHomePage(title: 'WaniKani for Mobile'),
     );
