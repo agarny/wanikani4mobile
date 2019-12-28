03448d8f257bba2ce60cd2392a9bf170a6f80fb0
Alan Garny <agarny@hellix.com>
2019-12-29 00:17:20 +1300

    Basic home page.

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WaniKani for Mobile'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Text('Welcome to WaniKani for Mobile!'),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:wanikani4mobile/home_page.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+void main() => runApp(new MaterialApp(
       title: 'WaniKani for Mobile',
       theme: ThemeData(
         primarySwatch: MaterialColor(0xFF294DDC, <int, Color>{
@@ -21,54 +18,5 @@ class MyApp extends StatelessWidget {
           900: Color.fromRGBO(41, 77, 220, 1.0),
         }),
       ),
-      home: MyHomePage(title: 'WaniKani for Mobile'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      ++_counter;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+      home: HomePage(),
+    ));
