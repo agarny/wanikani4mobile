08d8ea8470a14e5829eb425dd86593da4adca9da
Alan Garny <agarny@hellix.com>
2019-12-31 00:53:50 +1300

    WaniKani API token: can now log in by manually providing an API token.

--- lib/wanikani_api_token.dart
+++ lib/wanikani_api_token.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
+import 'package:provider/provider.dart';
+import 'package:wanikani4mobile/settings.dart';
 import 'package:wanikani4mobile/utilities.dart';
 
 class WaniKaniApiTokenPage extends StatefulWidget {
@@ -8,17 +10,22 @@ class WaniKaniApiTokenPage extends StatefulWidget {
 }
 
 class WaniKaniApiTokenPageState extends State<WaniKaniApiTokenPage> {
+  static const mask = '########-####-####-####-############';
+  final inputFormatter = MaskTextInputFormatter(
+    mask: mask,
+    filter: {
+      "#": RegExp(r'[0-9a-f]'),
+      "-": RegExp(r'\-'),
+    },
+  );
+  final _apiTokenRegExpr =
+      RegExp(r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$');
+
+  String _apiKey;
+  bool _buttonEnabled = false;
+
   @override
   Widget build(BuildContext context) {
-    const mask = '########-####-####-####-############';
-    final inputFormatter = MaskTextInputFormatter(
-      mask: mask,
-      filter: {
-        "#": RegExp(r'[0-9a-f]'),
-        "-": RegExp(r'\-'),
-      },
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Enter my API token'),
@@ -39,6 +46,25 @@ class WaniKaniApiTokenPageState extends State<WaniKaniApiTokenPage> {
                 hintText: mask,
               ),
               inputFormatters: [inputFormatter],
+              onChanged: (String value) {
+                if (_apiTokenRegExpr.hasMatch(value) != _buttonEnabled) {
+                  setState(() {
+                    _apiKey = value;
+                    _buttonEnabled = !_buttonEnabled;
+                  });
+                }
+              },
+            ),
+            OutlineButton(
+              child: Text('Log in'),
+              onPressed: _buttonEnabled
+                  ? () {
+                      Provider.of<Settings>(context, listen: false)
+                          .setApiToken(_apiKey);
+
+                      goToPage(HomeRoute, context);
+                    }
+                  : null,
             ),
           ],
         ),
