b6b9dd1a925236c5a6eb2fc371369d6a50673de0
Alan Garny <agarny@hellix.com>
2020-01-05 12:40:59 +1300

    Home: show a user's gravatar or a default one, if s/he doesn't have one.

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
 import 'package:flutter/material.dart';
+import 'package:wanikani4mobile/settings.dart';
 import 'package:wanikani4mobile/utilities.dart';
 import 'package:wanikani4mobile/wanikani.dart';
 
@@ -8,23 +13,45 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  static const _defaultGravatar = 'https://aws1.discourse-cdn.com/business5/uploads/wanikani_community/original/3X/f/d/fd4c154120954695f788402f3bcf4e616499bc2d.png';
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<WaniKani>(
         future: WaniKani().fetch(),
         builder: (context, snapshot) {
+          var gravatarSize = 0.75*min(AppBar().preferredSize.width, AppBar().preferredSize.height);
+          var hash =
+              md5.convert(utf8.encode(Settings().emailAddress)).toString();
+
           return Scaffold(
             appBar: AppBar(
               title: (!WaniKani().initialized || WaniKani().hasError)
                   ? Text('WaniKani for Mobile')
-                  : Column(
+                  : Row(
                       children: <Widget>[
-                        Text(
-                          WaniKani().user.data.username,
-                          textScaleFactor: 1.5,
+                         Container(
+                            width: gravatarSize,
+                            height: gravatarSize,
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: new NetworkImage(
+                                        "https://www.gravatar.com/avatar/$hash.jpg?d=$_defaultGravatar")
+                                )
+                            )),
+                        SizedBox(width: space(context)),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              WaniKani().user.data.username,
+                              textScaleFactor: 1.5,
+                            ),
+                            Text('Level ${WaniKani().user.data.level}',
+                                textScaleFactor: 0.75),
+                          ],
                         ),
-                        Text('Level ${WaniKani().user.data.level}',
-                            textScaleFactor: 0.75),
                       ],
                     ),
               actions: <Widget>[
