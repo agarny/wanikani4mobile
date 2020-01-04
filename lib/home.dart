dc4c19cc5fb2e0070dd7eb3414bc79441a5766d6
Alan Garny <agarny@hellix.com>
2020-01-05 11:21:05 +1300

    Home: show the name of the user and his/her level in our app bar.

--- lib/home.dart
+++ lib/home.dart
@@ -8,58 +8,58 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  Widget containerChild() {
-    return (WaniKani().hasError)
-        ? Text(WaniKani().errorMessage)
-        : RefreshIndicator(
-            child: ListView(
-              children: <Widget>[
-                Text(
-                    'Welcome to WaniKani for Mobile ${WaniKani().user.data.username}!\n'
-                    '\n'
-                    'Number of lessons available: ${WaniKani().summary.data.lessons[0].subjectIds.length}.\n'
-                    'Number of reviews available: ${WaniKani().summary.data.reviews[0].subjectIds.length}.'),
-              ],
-            ),
-            onRefresh: () async {
-              await WaniKani().fetch();
-
-              setState(() {});
-
-              return;
-            },
-          );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('WaniKani for Mobile'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              Navigation().navigateTo(SettingsRoute);
-            },
-          ),
-        ],
-      ),
-      drawer: drawer(context),
-      body: Container(
-        margin: EdgeInsets.all(space(context)),
-        child: WaniKani().initialized
-            ? containerChild()
-            : FutureBuilder<WaniKani>(
-                future: WaniKani().fetch(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return containerChild();
-                  }
+    return FutureBuilder<WaniKani>(
+        future: WaniKani().fetch(),
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: AppBar(
+              title: (!WaniKani().initialized || WaniKani().hasError)
+                  ? Text('WaniKani for Mobile')
+                  : Column(
+                      children: <Widget>[
+                        Text(
+                          WaniKani().user.data.username,
+                          textScaleFactor: 1.5,
+                        ),
+                        Text('Level ${WaniKani().user.data.level}',
+                            textScaleFactor: 0.75),
+                      ],
+                    ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.settings),
+                  onPressed: () {
+                    Navigation().navigateTo(SettingsRoute);
+                  },
+                ),
+              ],
+            ),
+            drawer: drawer(context),
+            body: Container(
+                margin: EdgeInsets.all(space(context)),
+                child: (!WaniKani().initialized)
+                    ? Text('Please wait while we are reetrieving your data...')
+                    : (WaniKani().hasError)
+                        ? Text(WaniKani().errorMessage)
+                        : RefreshIndicator(
+                            child: ListView(
+                              children: <Widget>[
+                                Text(
+                                    'Number of lessons available: ${WaniKani().summary.data.lessons[0].subjectIds.length}.\n'
+                                    'Number of reviews available: ${WaniKani().summary.data.reviews[0].subjectIds.length}.'),
+                              ],
+                            ),
+                            onRefresh: () async {
+                              await WaniKani().fetch();
 
-                  return Text('Please wait while we are fetching the data...');
-                }),
-      ),
-    );
+                              setState(() {});
+
+                              return;
+                            },
+                          )),
+          );
+        });
   }
 }
