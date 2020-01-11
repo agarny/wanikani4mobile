744a3759e0e6d275f3bfb8f097093ad70154628c
Alan Garny <agarny@hellix.com>
2020-01-11 13:32:43 +1300

    WaniKani API: allow for nulls in our level_progressions API.

--- lib/wanikani/level_progressions.dart
+++ lib/wanikani/level_progressions.dart
@@ -19,22 +19,31 @@ class WaniKaniLevelProgressions {
 
   factory WaniKaniLevelProgressions.fromJson(Map<String, dynamic> json) =>
       WaniKaniLevelProgressions(
-        object: json["object"],
-        url: json["url"],
-        pages: WaniKaniPages.fromJson(json["pages"]),
-        totalCount: json["total_count"],
-        dataUpdatedAt: DateTime.parse(json["data_updated_at"]),
-        data: List<WaniKaniLevelProgressionsData>.from(
-            json["data"].map((x) => WaniKaniLevelProgressionsData.fromJson(x))),
+        object: json["object"] == null ? null : json["object"],
+        url: json["url"] == null ? null : json["url"],
+        pages: json["pages"] == null
+            ? null
+            : WaniKaniPages.fromJson(json["pages"]),
+        totalCount: json["total_count"] == null ? null : json["total_count"],
+        dataUpdatedAt: json["data_updated_at"] == null
+            ? null
+            : DateTime.parse(json["data_updated_at"]),
+        data: json["data"] == null
+            ? null
+            : List<WaniKaniLevelProgressionsData>.from(json["data"]
+                .map((x) => WaniKaniLevelProgressionsData.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
-        "object": object,
-        "url": url,
-        "pages": pages.toJson(),
-        "total_count": totalCount,
-        "data_updated_at": dataUpdatedAt.toIso8601String(),
-        "data": List<dynamic>.from(data.map((x) => x.toJson())),
+        "object": object == null ? null : object,
+        "url": url == null ? null : url,
+        "pages": pages == null ? null : pages.toJson(),
+        "total_count": totalCount == null ? null : totalCount,
+        "data_updated_at":
+            dataUpdatedAt == null ? null : dataUpdatedAt.toIso8601String(),
+        "data": data == null
+            ? null
+            : List<dynamic>.from(data.map((x) => x.toJson())),
       };
 }
 
@@ -55,19 +64,25 @@ class WaniKaniLevelProgressionsData {
 
   factory WaniKaniLevelProgressionsData.fromJson(Map<String, dynamic> json) =>
       WaniKaniLevelProgressionsData(
-        id: json["id"],
-        object: objectValues.map[json["object"]],
-        url: json["url"],
-        dataUpdatedAt: DateTime.parse(json["data_updated_at"]),
-        data: WaniKaniLevelProgressionsDataData.fromJson(json["data"]),
+        id: json["id"] == null ? null : json["id"],
+        object:
+            json["object"] == null ? null : objectValues.map[json["object"]],
+        url: json["url"] == null ? null : json["url"],
+        dataUpdatedAt: json["data_updated_at"] == null
+            ? null
+            : DateTime.parse(json["data_updated_at"]),
+        data: json["data"] == null
+            ? null
+            : WaniKaniLevelProgressionsDataData.fromJson(json["data"]),
       );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "object": objectValues.reverse[object],
-        "url": url,
-        "data_updated_at": dataUpdatedAt.toIso8601String(),
-        "data": data.toJson(),
+        "id": id == null ? null : id,
+        "object": object == null ? null : objectValues.reverse[object],
+        "url": url == null ? null : url,
+        "data_updated_at":
+            dataUpdatedAt == null ? null : dataUpdatedAt.toIso8601String(),
+        "data": data == null ? null : data.toJson(),
       };
 }
 
@@ -93,9 +108,13 @@ class WaniKaniLevelProgressionsDataData {
   factory WaniKaniLevelProgressionsDataData.fromJson(
           Map<String, dynamic> json) =>
       WaniKaniLevelProgressionsDataData(
-        createdAt: DateTime.parse(json["created_at"]),
-        level: json["level"],
-        unlockedAt: DateTime.parse(json["unlocked_at"]),
+        createdAt: json["created_at"] == null
+            ? null
+            : DateTime.parse(json["created_at"]),
+        level: json["level"] == null ? null : json["level"],
+        unlockedAt: json["unlocked_at"] == null
+            ? null
+            : DateTime.parse(json["unlocked_at"]),
         startedAt: json["started_at"] == null
             ? null
             : DateTime.parse(json["started_at"]),
@@ -109,9 +128,9 @@ class WaniKaniLevelProgressionsDataData {
       );
 
   Map<String, dynamic> toJson() => {
-        "created_at": createdAt.toIso8601String(),
-        "level": level,
-        "unlocked_at": unlockedAt.toIso8601String(),
+        "created_at": createdAt == null ? null : createdAt.toIso8601String(),
+        "level": level == null ? null : level,
+        "unlocked_at": unlockedAt == null ? null : unlockedAt.toIso8601String(),
         "started_at": startedAt == null ? null : startedAt.toIso8601String(),
         "passed_at": passedAt == null ? null : passedAt.toIso8601String(),
         "completed_at": completedAt,
