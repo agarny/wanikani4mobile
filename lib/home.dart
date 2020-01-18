6ed31744a132acd62027074b9ed221cb33b40db1
Alan Garny <agarny@hellix.com>
2020-01-18 17:07:22 +1300

    Show the refresh indicator when fetching our data at startup.

--- lib/home.dart
+++ lib/home.dart
@@ -117,86 +117,90 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static final GlobalKey<RefreshIndicatorState> _refreshIndicatorState =
+      new GlobalKey<RefreshIndicatorState>();
   static const _defaultGravatar =
       'https://aws1.discourse-cdn.com/business5/uploads/wanikani_community/original/3X/f/d/fd4c154120954695f788402f3bcf4e616499bc2d.png';
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback(
+        (_) => _refreshIndicatorState.currentState.show());
+  }
 
   @override
   Widget build(BuildContext context) {
     var hash = md5.convert(utf8.encode(Settings().emailAddress)).toString();
 
-    return FutureBuilder<WaniKani>(
-        future: WaniKani().fetch(),
-        builder: (context, snapshot) {
-          return Scaffold(
-            appBar: AppBar(
-              automaticallyImplyLeading: false,
-              titleSpacing: space(context),
-              title: WaniKani().hasError
-                  ? Text('WaniKani for Mobile')
-                  : Row(
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: CachedNetworkImageProvider(
-                              'https://www.gravatar.com/avatar/$hash.jpg?d=$_defaultGravatar'),
-                        ),
-                        SizedBox(width: space3x(context)),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              WaniKani().userName(),
-                              style: Theme.of(context).accentTextTheme.headline,
-                            ),
-                            Text(
-                              WaniKani().level(),
-                              style: Theme.of(context).accentTextTheme.caption,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-              actions: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.settings),
-                  onPressed: () {
-                    Navigation().navigateTo(SettingsRoute);
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        titleSpacing: space(context),
+        title: WaniKani().hasError
+            ? Text('WaniKani for Mobile')
+            : Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(
+                        'https://www.gravatar.com/avatar/$hash.jpg?d=$_defaultGravatar'),
+                  ),
+                  SizedBox(width: space3x(context)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        WaniKani().userName(),
+                        style: Theme.of(context).accentTextTheme.headline,
+                      ),
+                      Text(
+                        WaniKani().level(),
+                        style: Theme.of(context).accentTextTheme.caption,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              Navigation().navigateTo(SettingsRoute);
+            },
+          ),
+        ],
+      ),
+      drawer: drawer(context),
+      body: Container(
+          child: (WaniKani().hasError)
+              ? Text(WaniKani().errorMessage)
+              : RefreshIndicator(
+                  key: _refreshIndicatorState,
+                  child: ListView(
+                    children: <Widget>[
+                      thinDivider(),
+                      _header(context, 'CURRENTLY AVAILABLE'),
+                      _currentlyAvailable(context, _CurrentlyAvailable.Lessons),
+                      thinDivider(),
+                      _currentlyAvailable(context, _CurrentlyAvailable.Reviews),
+                      _header(context, 'UPCOMING REVIEWS'),
+                      _upcomingReviews(context, _UpcomingReviews.NextHour),
+                      thinDivider(),
+                      _upcomingReviews(context, _UpcomingReviews.NextDay),
+                      thinDivider(),
+                      _upcomingReviews(context, _UpcomingReviews.All),
+                      thinDivider(),
+                    ],
+                  ),
+                  onRefresh: () async {
+                    await WaniKani().fetch();
+
+                    setState(() {});
+
+                    return;
                   },
-                ),
-              ],
-            ),
-            drawer: drawer(context),
-            body: Container(
-                child: (WaniKani().hasError)
-                    ? Text(WaniKani().errorMessage)
-                    : RefreshIndicator(
-                        child: ListView(
-                          children: <Widget>[
-                            thinDivider(),
-                            _header(context, 'CURRENTLY AVAILABLE'),
-                            _currentlyAvailable(
-                                context, _CurrentlyAvailable.Lessons),
-                            thinDivider(),
-                            _currentlyAvailable(
-                                context, _CurrentlyAvailable.Reviews),
-                            _header(context, 'UPCOMING REVIEWS'),
-                            _upcomingReviews(
-                                context, _UpcomingReviews.NextHour),
-                            thinDivider(),
-                            _upcomingReviews(context, _UpcomingReviews.NextDay),
-                            thinDivider(),
-                            _upcomingReviews(context, _UpcomingReviews.All),
-                            thinDivider(),
-                          ],
-                        ),
-                        onRefresh: () async {
-                          await WaniKani().fetch();
-
-                          setState(() {});
-
-                          return;
-                        },
-                      )),
-          );
-        });
+                )),
+    );
   }
 }
